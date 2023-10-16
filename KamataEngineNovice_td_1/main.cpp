c5f98cea6bee83edc549447cb52ce6e7f569511a
K023g <K023g@MSI>
2023-10-16 16:27:07 +0900

    いろいろ

--- KamataEngineNovice_td_1/main.cpp
+++ KamataEngineNovice_td_1/main.cpp
@@ -34,23 +34,30 @@ typedef struct Trap//トラップの変数
 	EdgePosition edgePosition;
 }Trap;
 
+typedef struct Goal//ゴールの変数
+{
+	Vector2 position;
+	float newPosY;
+	float radius;
+	EdgePosition edgePosition;
+}Goal;
+
 typedef struct BackPage
 {
-	Vector2 pos1;
-	Vector2 pos2;
+	Vector2 pos1;//一個減らした
 }BackPage;
-
-typedef struct JumpPower
-{
-	float first;
-	float second;
-}jumpPower;
 
 typedef struct DogSpeed
 {
 	float normal;
 	float slow;
 }DogSpeed;
+
+typedef struct JumpPower
+{
+	float low;
+	float high;
+}JumpPower;
 
 typedef struct Scene
 {
@@ -61,7 +68,7 @@ typedef struct Scene
 
 const int world = 500;
 
-const int worldEnd = 8000;
+const int worldEnd = 1280*4;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -70,8 +77,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	srand((unsigned int)time(NULL));//乱数の初期化
 
@@ -87,10 +94,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		30.0f,
 	};
 
-	JumpPower jumpPower//ジャンプ力
+	JumpPower jumpPower =
 	{
-		20.0f,15.0f
+		15.0f,//低いとき
+		20.0f//高いとき
 	};
+
+	const int jumpBottanPower = 60;
+
+	int jumpBottanCount = 0;
 
 	DogSpeed dogSpeed//速さ
 	{
@@ -105,32 +117,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//========================================
 	// 箱に関する変数
 	//========================================
-	/*Trap trap1;
-	
-	trap1.radius = 90.0f;
-
-	trap1.position = { 1000.0f,trap1.radius};
-	trap1.newPosY = (trap1.position.y - world) * -1.0f;
-	
-	trap1.edgePosition.right = trap1.position.x + trap1.radius;
-	trap1.edgePosition.left = trap1.position.x - trap1.radius;
-	trap1.edgePosition.up = trap1.position.y + trap1.radius;
-	trap1.edgePosition.down = trap1.position.y - trap1.radius;
-
-	
-	
-	Trap trap2;
-
-	trap2.radius = 90.0f;
-
-	trap2.position = { 1500.0f,trap2.radius * -1 + 1};
-	trap2.newPosY = (trap2.position.y - world) * -1.0f;
-
-	trap2.edgePosition.right = trap2.position.x + trap2.radius;
-	trap2.edgePosition.left = trap2.position.x - trap2.radius;
-	trap2.edgePosition.up = trap2.position.y + trap2.radius;
-	trap2.edgePosition.down = trap2.position.y - trap2.radius;*/
-
 	const int maxTrap = 10;
 
 	Trap trap[maxTrap];
@@ -139,7 +125,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	{
 		trap[i].radius = 90.0f;
 
-		trap[i].position = {(float)((worldEnd / maxTrap + rand() % 400 + 100) * (i + 1)),(float)(trap[i].radius * -1 + 1)}; 
+		trap[i].position = { (float)((worldEnd / maxTrap + rand() % 200) * (i + 1)),(float)(trap[i].radius * -1 + 1) };
 		trap[i].newPosY = (trap[i].position.y - world) * -1.0f;
 
 		trap[i].edgePosition.right = trap[i].position.x + trap[i].radius;
@@ -158,17 +144,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	BackPage backPage
 	{
-		{    0.0f, 0.0f,},
-		{-1280.0f, 0.0f,},
+		{0.0f, 0.0f}//一個減らした
 	};
+	//========================================
+	//========================================
+
+	//========================================
+	// ゴール用の変数
+	//========================================
+	Goal goal;
+	goal.radius = 128;
+
+	goal.position = { worldEnd - 200, goal.radius };
 	//========================================
 	//========================================
 
 	//========================================
 	// その他の変数
 	//========================================
+	int a = 0;
 
-	
 
 	//========================================
 	//========================================
@@ -178,7 +173,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//========================================
 	int gameBackGround = Novice::LoadTexture("./resources/BackGround/BackGround.png");
 
-	
+
 	//========================================
 	//========================================
 	// ウィンドウの×ボタンが押されるまでループ
@@ -202,57 +197,47 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{
 			playerHitCount--;
 		}
-		
-		
-		
+
+
+
 		//========================================
 		//========================================
 
 		//========================================
 		// プレイヤーの移動に関する処理
 		//========================================
-		if (keys[DIK_A] != 0)
+		if (playerHitCount == 0)//移動速度低下時の判定の仕方を変更
 		{
-			if (playerHitCount == 0)
-			{
-				player.velocity.x = dogSpeed.normal * -1;
-			}
-			if (playerHitCount != 0)
-			{
-				player.velocity.x = dogSpeed.slow * -1;
-			}
+			player.velocity.x = dogSpeed.normal;
 		}
-		else if (keys[DIK_D] != 0)
+		else if (playerHitCount != 0)
 		{
-			if (playerHitCount == 0)
-			{
-				player.velocity.x = dogSpeed.normal;
-			}
-			if (playerHitCount != 0)
-			{
-				player.velocity.x = dogSpeed.slow;
-			}
+			player.velocity.x = dogSpeed.slow;
+		}
+
+		if (keys[DIK_A] != 0 || keys[DIK_LEFT])
+		{
+			player.worldPosition.x -= player.velocity.x;
+		}
+		else if (keys[DIK_D] != 0 || keys[DIK_RIGHT])
+		{
+			player.worldPosition.x += player.velocity.x;
 		}
 		else
 		{
 			player.velocity.x = 0;
 		}
 
-		player.worldPosition.x += player.velocity.x;
 		//==============
 		//背景用
 		//==============
-		backPage.pos1.x -= player.velocity.x;
-		backPage.pos2.x -= player.velocity.x;
+		if (player.worldPosition.x >= 640 && player.worldPosition.x <= 4480)
+		{
+			backPage.pos1.x = player.worldPosition.x - 640;
+		}
 
-		if (backPage.pos1.x >= 1280)
-		{
-			backPage.pos1.x = 1280 * 2;
-		}
-		if (backPage.pos2.x >= 1280)
-		{
-			backPage.pos2.x = 1280 * 2;
-		}
+
+		
 		//==============
 		//==============
 
@@ -270,18 +255,33 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		if (player.worldPosition.y > player.radius)
 		{
 			player.velocity.y += player.accelecion.y;
-			
+
 		}
 		else if (player.worldPosition.y < player.radius)
 		{
 			player.worldPosition.y = player.radius;
 			player.velocity.y = 0.0f;
+			jumpBottanCount = 0;
 		}
 		else if (player.worldPosition.y == player.radius)
 		{
-			if ((keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] == 0))
+			if ((keys[DIK_SPACE] != 0 && preKeys[DIK_SPACE] != 0))//スペースキーを押している間
 			{
-				player.velocity.y = jumpPower.first;
+				jumpBottanCount++;
+			}
+			else if ((keys[DIK_SPACE] == 0 && preKeys[DIK_SPACE] != 0))//スペースキーを離したとき
+			{
+				if (jumpBottanCount < jumpBottanPower)//ジャンプ力が低いとき
+				{
+					player.velocity.y = jumpPower.low;
+					a = 1;
+
+				}
+				else if (jumpBottanCount >= jumpBottanPower)//ジャンプ力が高いとき
+				{
+					player.velocity.y = jumpPower.high;
+					a = 2;
+				}
 			}
 		}
 
@@ -310,34 +310,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		player.edgePosition.up = player.worldPosition.y + player.radius;
 		player.edgePosition.down = player.worldPosition.y - player.radius;
 
-		//if ((player.edgePosition.right <= trap1.edgePosition.left) == 0)//boxより右にいたら
-		//{
-		//	if ((player.edgePosition.down >= trap1.edgePosition.up) == 0)//boxより上に来たら
-		//	{
-		//		if ((player.edgePosition.left >= trap1.edgePosition.right) == 0)
-		//		{
-		//			playerHitCount = 120;
-		//		}
-		//	}
-		//}
-
-		//if ((player.edgePosition.right <= trap2.edgePosition.left) == 0)//boxより右にいたら
-		//{
-		//	if ((player.edgePosition.down >= trap2.edgePosition.up) == 0)//boxより上に来たら
-		//	{
-		//		if ((player.edgePosition.left >= trap2.edgePosition.right) == 0)
-		//		{
-		//			playerHitCount = 120;
-		//		}
-		//	}
-		//}
+		
 		for (int i = 0; i < maxTrap; i++)
 		{
-			if ((player.edgePosition.right <= trap[i].edgePosition.left) == 0)//boxより右にいたら
+			if (player.edgePosition.right >= trap[i].edgePosition.left)//boxより右にいたら
 			{
-				if ((player.edgePosition.down >= trap[i].edgePosition.up) == 0)//boxより上に来たら
+				if (player.edgePosition.down <= trap[i].edgePosition.up)//boxより上に来たら
 				{
-					if ((player.edgePosition.left >= trap[i].edgePosition.right) == 0)
+					if (player.edgePosition.left <= trap[i].edgePosition.right)
 					{
 						playerHitCount = 120;
 					}
@@ -359,24 +339,32 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		Novice::DrawSprite
-		(
-			(int)backPage.pos1.x, (int)backPage.pos1.y,
-			gameBackGround, 1, 1, 0.0f, WHITE
-		);
+		/// 背景
+		/*Novice::DrawSprite
+		(0-(int)backPage.pos1.x, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		(0-(int)backPage.pos1.x+1280, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		(0-(int)backPage.pos1.x+1280*2, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		(0-(int)backPage.pos1.x+1280*3, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		(0-(int)backPage.pos1.x+1280*4, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);*/
 
-		Novice::DrawSprite
-		(
-			(int)backPage.pos2.x, (int)backPage.pos2.y,
-			gameBackGround, 1, 1, 0.0f, WHITE
-		);
-		
-		Novice::DrawBox
+		Novice::DrawSprite(0 - (int)backPage.pos1.x, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		Novice::DrawSprite(0 - (int)backPage.pos1.x + 1280, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		Novice::DrawSprite(0 - (int)backPage.pos1.x + 1280 * 2, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		Novice::DrawSprite(0 - (int)backPage.pos1.x + 1280 * 3, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+		Novice::DrawSprite(0 - (int)backPage.pos1.x + 1280 * 4, (int)backPage.pos1.y, gameBackGround, 1, 1, 0.0f, WHITE);
+
+
+		//自機
+		Novice::DrawEllipse((int)player.worldPosition.x - (int)backPage.pos1.x, (int)player.newPosY, 50, 50, 0.0f, GREEN, kFillModeSolid);
+
+
+
+		/*Novice::DrawBox
 		(
 			(int)(player.worldPosition.x - (player.radius + scrollPosX)), (int)(player.newPosY - player.radius),
 			(int)player.radius * 2, (int)player.radius * 2,
 			0.0f, RED, kFillModeSolid
-		);
+		);*/
 
 		/*Novice::DrawBox
 		(
@@ -391,7 +379,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			(int)trap2.radius * 2, (int)trap2.radius * 2,
 			0.0f, BLUE, kFillModeSolid
 		);*/
-	
+
 		for (int i = 0; i < maxTrap; i++)
 		{
 			Novice::DrawBox
@@ -402,8 +390,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			);
 		}
 
-		Novice::ScreenPrintf(0, 0, "x1 %.0f, x2 %.0f",backPage.pos1.x, backPage.pos2.x);
+		//Novice::ScreenPrintf(0, 0, "x1 %.0f, x2 %.0f",backPage.pos1.x, backPage.pos2.x);
 		Novice::ScreenPrintf(0, 20, "%d", playerHitCount);
+
+		for (int i = 0; i < maxTrap; i++)
+		{
+			Novice::ScreenPrintf(i * 40, 40, "%.0f", trap[i].position.x - (trap[i].radius));
+		}
+
+		if (a == 1)
+		{
+			Novice::ScreenPrintf(0, 60, "low");
+		}
+		if (a == 2)
+		{
+			Novice::ScreenPrintf(0, 60, "high");
+		}
+
+		Novice::ScreenPrintf(0, 0, "(%d)", (int)player.worldPosition.x);
 		///
 		/// ↑描画処理ここまで
 		///
